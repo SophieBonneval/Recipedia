5b26b1ed02bdc79b20a3eea4274287f61b455a6d
Sophie Bonneval <maldorana@gmail.com>
2021-11-02 10:49:24 +0000

    First tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, cleanup } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders greeting to the world", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const greeting = screen.getByText(/Hello world/i);
+  expect(greeting).toBeInTheDocument();
+});
+
+test("renders greeting to the world 2", () => {
+  render(<App />);
+  const greeting = screen.getByTestId("test-greeting");
+  expect(greeting).toBeInTheDocument();
+  expect(greeting).toHaveTextContent("Hello");
 });
