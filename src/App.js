c022172f6cc491423c3f7f69e5bf85ab61087e30
amanda vuong <mandyvuong@gmail.com>
2021-11-02 14:21:16 +0000

    searchbar and submit button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import React from 'react'
 import "./App.css";
 import Header from "./header/Header";
+import Searchbar from "./searchbar/Searchbar";
+
 
 function App() {
   return (
     <div className="App" data-testid="test-greeting">
       <Header />
+      <Searchbar />
       <p>Hello world</p>
     </div>
   );
