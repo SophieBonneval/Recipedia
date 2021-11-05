6857f4358b09c5d0cc60c1390f501b365bdb3f2c
amanda vuong <mandyvuong@gmail.com>
2021-11-05 16:50:06 +0000

    added api, but not working yet

--- src/components/RecipeDetail/Recipedetail.js
+++ src/components/RecipeDetail/Recipedetail.js
@@ -1,27 +1,31 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import { useParams } from 'react-router-dom'
 
 function RecipeDetail() {
   const { id } = useParams();
-  // const [recipeDetail, setRecipeDetail] = useState([]);
+  const [recipe, setRecipe] = useState(null);
 
-  // const url = `https://api.spoonacular.com/recipes/101/information?apiKey=${process.env.REACT_APP_API_KEY}&includeNutrition=false`;
-  // const fetchData = async () => {
-  //   try {
-  //     const response = await fetch(url);
-  //     const json = await response.json();
-  //     console.log(json.sourceUrl);
-  //     setRecipeDetail(json.sourceUrl);
-  //   } catch (error) {
-  //     console.log('error', error);
-  //   }
-  // };
+  useEffect(() => {
+    const url = `https://api.spoonacular.com/recipes/${id}/information?apiKey=${process.env.REACT_APP_API_KEY}&includeNutrition=false`;
 
-  // fetchData();
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        console.log(json);
+        setRecipe(json);
+      } catch (error) {
+        console.log('error', error);
+      }
+    };
+  
+    fetchData();
+}, []);
 
   return (
     <div>
     {console.log(id)}
+    {console.log(recipe)}
     {/* {recipeDetail && recipeDetail}
     recipe.id
     recipe.title
