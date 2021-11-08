624b73369c8b24ed60ff91517c50fdd69f8e6e1b
Sophie Bonneval <maldorana@gmail.com>
2021-11-08 11:52:00 +0000

    fix recipe details bug and make the external links open a new tab when opened

--- src/components/RecipeDetail/Recipedetail.js
+++ src/components/RecipeDetail/Recipedetail.js
@@ -1,5 +1,5 @@
-import React, {useEffect, useState} from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 function RecipeDetail() {
   const { id } = useParams();
@@ -18,32 +18,40 @@ function RecipeDetail() {
         console.log('error', error);
       }
     };
-  
-    fetchData();
-}, []);
 
-  if (recipe!== null) {
-  return (
-    <div>
-    <h1>{recipe.title}</h1>
-    <img src={recipe.image} alt={recipe.title} />
-    <ul>
-			<li>🍽 Serves: {recipe.servings}</li>
-			<li>⏱Total Time: {recipe.readyInMinutes} minutes</li>
-		</ul>
-    <h2> Ingredients </h2>
-    <ul>
-      {recipe.extendedIngredients.map((ingredient) => <li key={ingredient.id}>{ingredient.original}</li>)}
-    </ul>
-    <h2> Instructions </h2>
-    <ol>
-      {recipe.analyzedInstructions[0].steps.map((step) => <li key={step.id}>{step.step}</li>)}
-    </ol>
-    <a href={recipe.sourceUrl}>link to original recipe</a>
-    </div>
-  )} else {
-    return <div></div>
+    fetchData();
+  }, []);
+
+  if (recipe !== null) {
+    return (
+      <div>
+        <h1>{recipe.title}</h1>
+        <img src={recipe.image} alt={recipe.title} />
+        <ul>
+          <li>🍽 Serves: {recipe.servings}</li>
+          <li>⏱Total Time: {recipe.readyInMinutes} minutes</li>
+        </ul>
+        <h2> Ingredients </h2>
+        <ul>
+          {recipe.extendedIngredients.map((ingredient) => (
+            <li key={ingredient.id}>{ingredient.original}</li>
+          ))}
+        </ul>
+        <h2> Instructions </h2>
+        <ol>
+          {recipe.analyzedInstructions[0] &&
+            recipe.analyzedInstructions[0].steps.map((step) => (
+              <li key={step.id}>{step.step}</li>
+            ))}
+        </ol>
+        <a href={recipe.sourceUrl} target='_blank' rel='noopener noreferrer'>
+          link to original recipe
+        </a>
+      </div>
+    );
+  } else {
+    return <div></div>;
   }
 }
 
-export default RecipeDetail
+export default RecipeDetail;
