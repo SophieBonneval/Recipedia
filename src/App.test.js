081afbf3c69a7288fdff97af1977727f1b232440
jake <jakebs540@gmail.com>
2021-11-09 16:19:28 +0000

    footer name tags take you to team members github pages

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,11 @@
-import { render, screen, cleanup } from "@testing-library/react";
-import React from "react";
-import App from "./App";
+import React from 'react';
+import { render, screen, cleanup } from '@testing-library/react';
+import App from './App';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-afterEach(() => {
-  cleanup();
-});
+  afterEach(() => {
+    cleanup();
+  });
 
 test(".nav element to be on the page", () => {
   render(<App />);
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -7,9 +7,9 @@ const Footer = () => {
     <footer>
     <div data-testid="Footer-tag"/>
       <p>
-        Created with <FaHeart className='heart' /> by <a href='#'>Jake</a>,{' '}
-        <a href='#'>Keldra</a>, <a href='#'>Amanda</a>, <a href='#'>Boris</a>{' '}
-        and <a href='#'>Sophie</a>
+        Created with <FaHeart className='heart' /> by <a href='https://github.com/Jjake540'>Jake</a>,{' '}
+        <a href='https://github.com/KeldraSJ'>Keldra</a>, <a href='https://github.com/mandyvuong'>Amanda</a>, <a href='https://github.com/borisl16'>Boris</a>{' '}
+        and <a href='https://github.com/Maldorana'>Sophie</a>
       </p>
     </footer>
   );
