7d8b26fba4dd603781132f1b057146f4f8e4db32
jake <jakebs540@gmail.com>
2021-11-09 15:47:00 +0000

    Footer test passing

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -5,6 +5,7 @@ import { FaHeart } from 'react-icons/fa';
 const Footer = () => {
   return (
     <footer>
+    <div data-testid="Footer-tag"/>
       <p>
         Created with <FaHeart className='heart' /> by <a href='#'>Jake</a>,{' '}
         <a href='#'>Keldra</a>, <a href='#'>Amanda</a>, <a href='#'>Boris</a>{' '}
