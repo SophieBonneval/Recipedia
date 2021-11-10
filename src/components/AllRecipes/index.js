98a6a3a84c94fcc9459f0a8392b6eff5b9046312
jake <jakebs540@gmail.com>
2021-11-10 15:30:40 +0000

    user recipes display on homepage

--- src/components/AllRecipes/index.js
+++ src/components/AllRecipes/index.js
@@ -1,7 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import './index.css';
+import { NavLink as Link } from 'react-router-dom';
+import styled from 'styled-components';
 import { collection, query, onSnapshot } from 'firebase/firestore';
 import { db } from '../../firebase-config';
+
+const NavLink = styled(Link)`
+  color: #393939;
+  text-decoration: none;
+  height: 100%;
+  cursor: pointer;
+
+  &:hover {
+    color: #000;
+  }
+`;
 
 const AllRecipes = () => {
   const [recipes, setRecipes] = useState([]);
@@ -24,13 +37,21 @@ const AllRecipes = () => {
     <div className='container'>
       <div className='my-recipes'>
         <h1>Members recipes</h1>
-        <div>
-          {recipes.map((recipe) => (
-            <div key={recipe.id}>
-              <div>{recipe.title}</div>
+        <div className='row'>
+          <div className='col'>
+            {recipes.map((recipe) => (
+            <NavLink key={recipe.id} to={`/recipe/${recipe.id}`}>
+            <div className='card'>
+              <div className='card-image'>
               <img src={recipe.image} alt={recipe.title} />
+              </div>
+                <div className='card-body'>
+                <h1>{recipe.title}</h1>
+                </div>
             </div>
-          ))}
+            </NavLink>
+            ))}
+          </div>
         </div>
       </div>
     </div>
